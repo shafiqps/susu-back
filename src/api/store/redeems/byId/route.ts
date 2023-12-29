fb4f368004c330e9a04c5549b0dfe7bf7f2c65e8
shafiqps <shafiqaimanps@gmail.com>
2023-12-29 19:54:08 +0900

    Modifications to redems/byID route

--- src/api/store/redeems/byId/route.ts
+++ src/api/store/redeems/byId/route.ts
@@ -8,9 +8,13 @@ import type {
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     const redeemService: RedeemService = req.scope.resolve("redeemService");
     const loggedInUserId = req.user.customer_id; // Assuming you have a way to get the logged-in user's ID
-  
+    const config = {
+      skip: 0,
+      take: 20, // You can adjust pagination settings here
+      relations: ["rewards"] // Include any relations if needed
+    };
     try {
-      const redeems = await redeemService.listById({}, {}, loggedInUserId);
+      const redeems = await redeemService.listById({}, config, loggedInUserId);
       res.status(200).json({ redeems });
     } catch (error) {
       res.status(500).json({ error: error.message });
